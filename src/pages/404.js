c85a398544b2ec4ca9dc6a08ca98269be3dd3679
aliandor <alfonsogalang86@gmail.com>
2019-05-07 05:20:03 +0000

    404 test

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => (
   <Layout>
     <div>
       <h1>SORRY NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You've just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
   </Layout>
 )
