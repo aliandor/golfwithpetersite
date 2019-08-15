615bd2eec008ac97746c58022ebbc03cba22e4c4
alfonsogalang86 <alfonsogalang86@gmail.com>
2019-08-15 01:04:40 -0700

    SEO improvments

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout/layout'
 import Landing from '../components/landing/landing'
 import Main from '../components/main/main'
+import { Helmet } from "react-helmet";
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <title>Golf With Peter</title>
+      <meta name='description' content='Personalized 1-on-1 Golf Instruction. No matter your age or skill level, improve your game with Altadena Golf Course Head Professional Peter Barrow.' />
+    </Helmet>
     <Landing />
     <Main />
   </Layout>
