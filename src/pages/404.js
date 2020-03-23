4e70a169122c4dae419c166ceb0169858d106cc8
Alfonso Galang <alfonsogalang86@gmail.com>
2020-03-22 22:22:22 -0700

    .

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout/layout'
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>SORRY NOT FOUND</h1>
+      <h1>SORRY NOT FOUND.</h1>
       <p>You've just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
   </Layout>
